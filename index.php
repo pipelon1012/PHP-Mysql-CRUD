b8776ffbcb3c08763ab251ac5dc922c999ce6f49
pipe12 <terracota-3@hotmail.com>
2021-07-21 12:30:24 -0500

    PHP Mysql CRUD

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php include("db.php") ?>
+
+<?php include("includes/header.php")?>
+    
+<div class="container p-4">
+
+
+   <div class="row">
+
+       <div class="col-md-4">
+
+           <div class="card card-body">
+               
+               <form action=""  >
+                    <div class="form-group">
+                        
+                    </div>
+                  
+               </form>     
+
+           </div>
+
+       </div>
+
+       <div class="col-md-8">
+           
+           </div>
+
+   </div>
+
+</div>
+
+
+
+
+   <?php include("includes/footer.php")?>
