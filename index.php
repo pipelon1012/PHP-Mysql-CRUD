677610242d44fbe8038de013a437514618b377da
pipe12 <terracota-3@hotmail.com>
2021-07-24 15:26:25 -0500

    Lista de tareas

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+
+  
 <?php include("db.php") ?>
 
 <?php include("includes/header.php")?>
@@ -8,6 +10,16 @@
    <div class="row">
 
        <div class="col-md-4">
+
+
+       <?php  if(isset($_SESSION['message'])) { ?>
+
+        <div class="alert alert-<?= $_SESSION['message_type'];?> alert-dismissible fade show" role="alert">
+        <?=$_SESSION['message'] ?>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+
+        <?php  session_unset(); }  ?>
 
            <div class="card card-body">
                
@@ -28,9 +40,36 @@
 
        </div>
 
-      <!-- <div class="col-md-8">
+       <div class="col-md-8">
+
+         <table  class="table table-bordered">
+             <thead>
+                 <tr>
+                     <th>Title</th>
+                     <th>Description</th>
+                     <th>Created At</th>
+                     <th>Actions</th>
+                 </tr>
+             </thead>
+            <tbody>
+                <?php
+                    $query = "SELECT * FROM task";
+                    $result_tasks = mysqli_query($conn,$query);
+
+                    while($row = mysqli_fetch_array($result_tasks)) { ?>
+
+                    <tr>
+                        <td><?php echo $row['title'] ?></td>
+
+                    </tr>
+
+                   <?php }  ?>
+                
+                
+            </tbody>
+         </table>
            
-           </div> -->
+           </div> 
 
    </div>
 
